43179bd0eec2d05a6aea9e79e6a8147761a41556
Cooper White <cooper1808@gmail.com>
2020-09-28 07:26:13 +1000

    Problem1

---
+++ Problem1/prob1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include<numeric>
+
+
+int main(){
+
+    int number = 1000, i = 0, answer = 0;
+    std::vector<int> arr;
+
+    while(i < number){
+
+        if (i % 3 == 0){
+            arr.push_back(i);
+        }
+
+        else if (i % 5 == 0){
+            arr.push_back(i);
+        }
+        i++;
+    }
+
+    answer = std::accumulate(arr.begin(), arr.end(), 0);
+
+    std::cout<<answer<<std::endl;
+
+
+
+}
---
+++ Problem1/prob1.py
@@ -0,0 +1,17 @@
+
+number = 1000
+numArray = []
+answer = 0
+for i in range(number):
+    if i % 3 == 0:
+        numArray.append(i)
+
+    elif i % 5 == 0:
+        numArray.append(i)
+
+
+print(numArray)
+for num in numArray:
+    answer += num
+
+print(answer)
