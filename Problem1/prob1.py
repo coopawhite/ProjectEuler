3b6c136e33c95f1544e304f2a2bbd8835c7ec86c
Cooper White <cooper1808@gmail.com>
2020-09-28 07:31:38 +1000

    update

--- Problem1/prob1.py
+++ Problem1/prob1.py
@@ -1,17 +1,20 @@
 
-number = 1000
-numArray = []
-answer = 0
-for i in range(number):
-    if i % 3 == 0:
-        numArray.append(i)
+#number = 1000
+#numArray = []
+#answer = 0
 
-    elif i % 5 == 0:
-        numArray.append(i)
+#for i in range(number):
+#    if i % 3 == 0:
+#        numArray.append(i)
+
+#    elif i % 5 == 0:
+#        numArray.append(i)
 
 
-print(numArray)
-for num in numArray:
-    answer += num
+#print(numArray)
+#for num in numArray:
+#    answer += num
+
+answer = sum(i for i in range(1000) if i%3 == 0 or i % 5 == 0)
 
 print(answer)
